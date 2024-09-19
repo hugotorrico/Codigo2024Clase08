581bff0aa4369ebbd2e0e9bb1e4116d023e2545f
Hugo Torrico <torricomarquezh@gmail.com>
2024-09-18 19:46:53 -0500

    Agregar archivos de proyecto.

---
+++ CodigoClase08/Profesor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigoClase08
+{
+    //Las clases singular
+    public class Profesor
+    {
+
+        #region Propiedades
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public double Sueldo { get; set; }
+        //prop + tap y se genera la propiedad
+        public string  TipoDocumento { get; set; }
+        public int EscalaSalarial { get; set; }
+
+        #endregion
+
+        //No tengo una propiedad llamada nombres completos
+        public string ObtenerNombresCompletos()
+        {
+            return Nombres + " " + Apellidos;
+        }
+
+        //Si tengo una propiedad llamada asignar Sueldo
+        public void AsignarSueldo()
+        {
+            switch (EscalaSalarial)
+            {
+                case 1:
+                    Sueldo = 1000;
+                    break;
+                case 2:
+                    Sueldo = 2000;
+                    break;
+                case 3:
+                    Sueldo = 3000;
+                    break;
+            }
+        }
+
+
+       
+       
+
+
+    }
+}
---
+++ CodigoClase08/Program.cs
@@ -0,0 +1,15 @@
+﻿using CodigoClase08;
+
+Profesor profesor = new Profesor
+{
+    Apellidos = "Torrico Márquez",
+    Nombres = "Hugo Felipe",
+    EscalaSalarial = 1,
+    TipoDocumento = "DNI"
+};
+
+
+profesor.AsignarSueldo();
+Console.WriteLine(profesor.Sueldo);
+
+Console.Read();
